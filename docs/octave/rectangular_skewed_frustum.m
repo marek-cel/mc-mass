de3279945434eed75673096473777b0fa95aca6c
Marek Cel <marek.cel@gmail.com>
2025-09-11 18:29:18 +0200

    Fix formatting of section headers in rectangular_skewed_frustum.m for consistency

--- docs/octave/rectangular_skewed_frustum.m
+++ docs/octave/rectangular_skewed_frustum.m
@@ -13,9 +13,19 @@ end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-disp('## Rectangular skewed frustum');
+disp('## Rectangular Skewed Frustum');
 
 disp('## Volume');
 
 syms lx1 ly1 lx2 ly2 lz
 syms vol
+
+disp('## Center of Mass');
+
+syms cg_x cg_y cg_z
+
+disp('## Moments of Inertia');
+
+syms x0 y0 z0 % 0th base center coordinates
+syms x1 y1 z1 % 1st base center coordinates
+syms Ixx Iyy Izz Ixy Ixz Iyz
